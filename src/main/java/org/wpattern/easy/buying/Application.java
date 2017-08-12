3c4e4cd81b6408cb72652d825ab5e74dfd134089
Augusto Branquinho <augustobranquinho@gmail.com>
2017-08-11 22:11:42 -0300

    Initial import.

---
+++ src/main/java/org/wpattern/easy/buying/Application.java
@@ -0,0 +1,24 @@
+package org.wpattern.easy.buying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping(path = "oi", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String oi() {
+        return "{}";
+    }
+
+}
