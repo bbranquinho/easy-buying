d71cd5565d8bb44b1c4a1a2a217706b32fcbbdb1
Augusto Branquinho <augustobranquinho@gmail.com>
2017-12-12 21:25:31 -0200

    Corrigindo o build.

--- src/main/java/org/wpattern/easy/buying/utils/AppConfig.java
+++ src/main/java/org/wpattern/easy/buying/utils/AppConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.wpattern.easy.buying.AppMain;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -52,6 +54,11 @@ public class AppConfig {
                 .description(appDescription)
                 .version(appVersion)
                 .build();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
     }
 
 }
